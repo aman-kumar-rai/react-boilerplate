f292d0ab735df82d4aefb28b81acd6ded88baa33
Aman <kumaraman140795@gmail.com>
2019-01-24 16:45:32 +0530

    Installed webpack-dev-server and configured it in dev mode

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import React from 'react';
 
 
 class App extends React.Component{
-
     render(){
         return (
-            <h1>I am a react developer...</h1>
+            <h1>So, this is working!!!</h1>
         );
     }
 }
-
 export default App;
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,6 +3,18 @@ const baseConfig = require('./webpack.config.base.js');
 
 // this is an object merge, so it is the same as Object.assign()...
 module.exports = merge(baseConfig, {
-    mode: "development"
+    mode: "development",
+    
+    // configurations for the development server
+    devServer: {
+        port: 8976,
+        open: true,
+        publicPath: '/',
+        // allows access to app using local IP(in LAN)
+        // useLocalIp: true,
+        // enables hot module replacement feature...
+        // hot: true,
+        // https: true
+    }
 });
 
